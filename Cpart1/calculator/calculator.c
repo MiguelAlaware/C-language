3fd75fb82db72d55bf04df10bee17eb8863c7e14
Miguel Fialho <80843682+MiguelAlaware@users.noreply.github.com>
2023-02-25 14:00:59 +0000

    Mais exemplos/exercícios criados

--- Cpart1/calculator/calculator.c
+++ Cpart1/calculator/calculator.c
@@ -4,10 +4,10 @@
 int main(void)    
 {
     //Usuário da input para x
-    int x = get_int("x: ");
+    float x = get_int("x: ");
     //Usuário da input para y
-    int y = get_int("y: ");
+    float y = get_int("y: ");
     //Faz a adição
-    int z = x + y;
-    printf("%i\n", z) ;
+    float z = (float) x / (float) y;
+    printf("%.50f\n", z) ;
 }
