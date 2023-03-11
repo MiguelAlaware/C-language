a37ab122dbfc20f27e829e110bd010845c84d13a
Miguel Fialho <80843682+MiguelAlaware@users.noreply.github.com>
2023-03-11 19:01:21 +0000

    Novos conceitos

---
+++ Cpart2/uppercase/uppercase.c
@@ -0,0 +1,22 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+int main(void)
+{
+    string s = get_string("input: ");
+    printf("Output: ");
+    for (int i = 0, n = strlen(s); i < n; i++)
+    {
+        if (islower(s[i]))
+        {
+            printf("%c", toupper(s[i]));
+        }
+        else
+        {
+            printf("%c", s[i]);
+        }
+    }
+    printf("\n");
+}
