678024af2876e264ee61af03c056aab8e7bf2f7f
Miguel Fialho <80843682+MiguelAlaware@users.noreply.github.com>
2023-04-05 19:04:03 +0000

    Novo material sobre pointers

---
+++ Cpart4/scanf/scanf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+
+    char *s = malloc(4);
+    printf("s: ");
+    scanf("%s", s);
+    printf("s: %s\n", s);
+
+    int x;
+    printf("x: ");
+    scanf("%i", &x);
+    printf("x: %i\n", x);
+
+}
